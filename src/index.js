7ff7bc3c50f422971d73b3fb3aa74742e47b6f16
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2025-09-17 09:47:24 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+app.use(express.json()); // for JSON body
